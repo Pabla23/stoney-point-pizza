7bd7ed59f80c1eebb551d4cdedb90d4160cbdc7e
Connor <cfroese4@my.bcit.ca>
2023-07-04 11:39:06 -0700

    initial commit

---
+++ front-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Stoney_Point_Pizza_Co.
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		the_title();
+
+		?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
