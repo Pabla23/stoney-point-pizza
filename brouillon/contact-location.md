5edfd8acae36d41c020ffb6a30ddca669b0b8337
pema37 <shawbernard281@gmail.com>
2023-07-19 22:26:46 -0700

    design complete for '<section class="contact-locations-section">'

---
+++ brouillon/contact-location.md
@@ -0,0 +1,57 @@
+// Make the page responsive for all screen sizes ( <=320 px; <=360px; <=480 px; <= 640 px ; <= 960 ; >= 1008px  )
+
+// <body class="page-template-default page page-id-31 wp-custom-logo theme-stoney-point woocommerce-js woocommerce-active"><script id="__bs_script__">
+
+
+<main id="primary" class="site-main">
+
+    <h1>Contact and Locations</h1>
+
+    <article id="post-31" class="post-31 page type-page status-publish hentry">
+        <header class="entry-header">
+            <h1 class="entry-title">Contact and Locations</h1>
+        </header>
+
+        <div class="entry-content">
+        </div>
+    </article>
+
+    <section class="contact-intro-section">
+        <p>Don't feel like going out tonight? We deliver!</p>
+        <a href="//10.65.18.106:8082/capstoneproject/shop/" class="order-now-btn btn"><p>Order Now</p></a>
+    </section>
+
+    <section class="contact-map-section">
+        <h2>Quick Finder</h2>
+    </section>
+
+
+    <section class="contact-locations-section">
+        <h2>Our Locations</h2>
+
+        <article>
+            <h3>Downtown</h3>
+            <a href="//10.65.18.106:8082/capstoneproject/shop/" class="order-article-btn btn"><p>Order</p></a>
+            <!-- other details... -->
+        </article>
+
+        <article>
+            <h3>Gastown</h3>
+            <a href="//10.65.18.106:8082/capstoneproject/shop/" class="order-article-btn btn"><p>Order</p></a>
+            <!-- other details... -->
+        </article>
+    </section>
+
+
+    <section class="contact-form-section">
+        <h2>Contact Us</h2>
+        <a href="//10.65.18.106:8082/capstoneproject/testimonials/"><p>Testimonial Archive</p></a>
+    </section>
+
+</main>
+
+
+
+
+
+
--- functions.php
+++ functions.php
@@ -245,20 +245,25 @@ add_action( 'admin_menu', 'hide_posts_from_admin_menu' );
 /**
  * STONEY POINT WOOCOMMERCE ENQUEUE SCRIPTS AND CSS
  */
+
+
+//  my version because of the previous error
 function enqueue_product_scripts() {
-	// enqueue single product scripts and styles
-	if ( is_product() ) :
+	// Enqueue single product scripts and styles
+	if ( is_singular( 'product' ) ) {
 		wp_enqueue_script( 'product-overlay-script', get_template_directory_uri() . '/js/product-overlay.js', array( 'jquery' ), '1.0', true );
-	endif;
-	// enqueue product archive scripts and styles
-	if ( is_post_type_archive( 'product' ) ) :
+	}
+	
+	// Enqueue product archive scripts and styles
+	if ( is_post_type_archive( 'product' ) ) {
 		wp_enqueue_script( 'product-archive-tabs', get_template_directory_uri() . '/js/product-archive-tabs.js', array( 'jquery' ), '1.0', true );
 		wp_enqueue_script( 'product-archive-slick', get_template_directory_uri() . '/js/start-slick.js', array( 'jquery' ), '1.0', true );
 		wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/js/slick.min.js', array( 'jquery' ), '1.0', true );
 		wp_enqueue_style( 'slick-css', get_template_directory_uri() . '/css/slick.css', array(), '1.0', 'all' );
-	endif;
+	}
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_product_scripts' );
+
 
 /**
  * STONEY POINT WOOCOMMERCE OVERRIDES
@@ -272,43 +277,42 @@ function woocommerce_template_loop_product_title() {
 	echo '<h3 class="' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '">' . get_the_title() . '</h3>';
 }
 
-// change product archive image size
+//  my version because of the previous error
 function woocommerce_template_loop_product_thumbnail() {
-	echo woocommerce_get_product_thumbnail('medium');
+	global $product;
+
+	echo $product->get_image('medium');
 }
+
+
+
+
 
 // change product bundle image size... code copied from plugin template then adjusted
 function custom_woocommerce_bundled_product_image_html($html, $product_id, $bundled_item) {
-    if ( has_post_thumbnail( $product_id ) ) {
-        $image_post_id = get_post_thumbnail_id( $product_id );
-        $image_title   = esc_attr( get_the_title( $image_post_id ) );
-        $image_data    = wp_get_attachment_image_src( $image_post_id, 'medium' ); // Change 'full' to 'medium' for the desired image size
-        $image_link    = $image_data[0];
-        $image         = wp_get_attachment_image( $image_post_id, 'medium', false, array(
-            'title'                   => $image_title,
-            'data-caption'            => get_post_field( 'post_excerpt', $image_post_id ),
-            'data-large_image'        => $image_link,
-            'data-large_image_width'  => $image_data[1],
-            'data-large_image_height' => $image_data[2],
-        ) );
+	if ( has_post_thumbnail( $product_id ) ) {
+			$image_post_id = get_post_thumbnail_id( $product_id );
+			$image_title   = esc_attr( get_the_title( $image_post_id ) );
+			$image_data    = wp_get_attachment_image_src( $image_post_id, 'medium' ); // Change 'full' to 'medium' for the desired image size
+			$image_link    = $image_data[0];
+			$image         = wp_get_attachment_image( $image_post_id, 'medium', false, array(
+					'title'                   => $image_title,
+					'data-caption'            => get_post_field( 'post_excerpt', $image_post_id ),
+					'data-large_image'        => $image_link,
+					'data-large_image_width'  => $image_data[1],
+					'data-large_image_height' => $image_data[2],
+			) );
 
-        $html  = '<figure class="bundled_product_image woocommerce-product-gallery__image">';
-        $html .= sprintf( '<a href="%1$s" class="image zoom" title="%2$s">%3$s</a>', $image_link, $image_title, $image );
-        $html .= '</figure>';
-    } else {
-        $html  = '<figure class="bundled_product_image woocommerce-product-gallery__image--placeholder">';
-        $html .= sprintf( '<a href="%1$s" class="placeholder_image zoom" data-rel="%3$s"><img class="wp-post-image" src="%1$s" alt="%2$s"/></a>', wc_placeholder_img_src(), __( 'Bundled product placeholder image', 'woocommerce-product-bundles' ), $image_rel );
-        $html .= '</figure>';
-    }
+			$html  = '<figure class="bundled_product_image woocommerce-product-gallery__image">';
+			$html .= sprintf( '<a href="%1$s" class="image zoom" title="%2$s">%3$s</a>', $image_link, $image_title, $image );
+			$html .= '</figure>';
+	} else {
+			$image_rel = '';
+			$html  = '<figure class="bundled_product_image woocommerce-product-gallery__image--placeholder">';
+			$html .= sprintf( '<a href="%1$s" class="placeholder_image zoom" data-rel="%3$s"><img class="wp-post-image" src="%1$s" alt="%2$s"/></a>', wc_placeholder_img_src(), __( 'Bundled product placeholder image', 'woocommerce-product-bundles' ), $image_rel );
+			$html .= '</figure>';
+	}
 
-    return $html;
+	return $html;
 }
 add_filter( 'woocommerce_bundled_product_image_html', 'custom_woocommerce_bundled_product_image_html', 10, 3 );
-
-// change product image size from thubmnail to medium in the cart
-function custom_woocommerce_cart_item_thumbnail( $product_get_image, $cart_item, $cart_item_key ) {
-	$product = $cart_item['data'];
-	$image = $product->get_image( 'medium' );
-	return $image;
-}
-add_filter( 'woocommerce_cart_item_thumbnail', 'custom_woocommerce_cart_item_thumbnail', 10, 3 );
